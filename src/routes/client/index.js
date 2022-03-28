6fd7fc22388fd96401a49aba61b71245ca787f3c
marcuspousette <marcuspousette@gmail.com>
2022-03-28 12:08:47 +0200

    remove unused code

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -1,7 +1,5 @@
 const app = require('../../loaders/express-handlebars');
 
-const { createOrder } = require('../../services/server/klarna');
-
 app.get('/', async function (req, res, next) {
 	// Replace with HTML snippet from CreateOrder Klarna Request
 	const html_snippet = `<h1>Now we can start to make some API calls</h1>`;
--- src/routes/server/order-lines.js
+++ src/routes/server/order-lines.js
@@ -1,7 +1,7 @@
 const app = require('../../loaders/express-handlebars');
 
+// THIS ROUTE IS FOR THOSE WHO WANT TO GO ABOVE AND BEYOND IN WEEK 10/11!
 app.get('/order-lines', function (req, res, next) {
-	// THIS ROUTE IS FOR THOSE WHO WANT TO GO ABOVE AND BEYOND IN WEEK 10/11!
 	res.send('Use this route to safely get cart information from a database');
 });
 
