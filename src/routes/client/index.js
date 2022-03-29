574576734c5e215cf49ef2b4f55c9908a1ef03db
marcuspousette <marcuspousette@gmail.com>
2022-03-29 12:48:22 +0200

    update single product checkout

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -6,7 +6,8 @@ app.get('/checkout/:product_id', async function (req, res, next) {
 	try {
 		const product_id = req.params.product_id;
 		const product = await getItemById(product_id);
-		const klarnaJsonResponse = await createOrder(product);
+		const products = [{ product, quantity: 1 }];
+		const klarnaJsonResponse = await createOrder(products);
 		const html_snippet = klarnaJsonResponse.html_snippet;
 		console.log('Order id: ', klarnaJsonResponse.order_id);
 		res.render('checkout', { klarna_checkout: html_snippet });
